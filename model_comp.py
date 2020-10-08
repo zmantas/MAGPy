78dc640f5721290b976662fcd1337da064385ff8
Christiaan <christiaanvanbuchem@gmail.com>
2020-10-08 17:43:02 +0200

    Added model_comp class en input data reading functionality

---
+++ model_comp.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+class model_comp():
+
+    def __init__(self,input_fname):
+
+        self.input_fname = input_fname
+
+        # Defining oxide names
+        self._oxide_names = "MgO",\
+                       "Al2O3",\
+                       "TiO2",\
+                       "Fe2O3",\
+                       "FeO",\
+                       "CaO",\
+                       "Na2O",\
+                       "K2O",\
+                       "Other"
+
+        # Reading data from input file
+        print(f'Reading input data from: {input_fname}')
+        model = np.genfromtxt(input_fname,dtype=None,delimiter='\n',encoding='UTF-8',skip_header=2)
+        
+        # Assigning wt values to corresponding oxide names
+        self.wt_oxides = {}
+        for i,name in enumerate(self._oxide_names):
+            self.wt_oxides[name] = np.float(model[i])
+
+    # end __init__()
+
+    def print(self):
+       
+        # Prints composition of the model
+        print(f'Model read from {self.input_fname} is composed of:')
+        for name, wt in self.wt_oxides.items():
+            print(f'{name}: {wt}')
+
+    # end print()
+
+# end model_comp()
